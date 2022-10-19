dd4b9570ca03294ae3a74cf0a314c9028d17c347
prefess <maicotion@gmail.com>
2022-10-19 10:15:29 +0700

    Chinh sua code test bo JUnit

---
+++ test/com/fu/mathutil/util/DataDrivenSelectionSortTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fu.mathutil.util;
+
+import static com.fu.mathutil.util.MathUtil.getFactorial;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author Thoai
+ */
+public class DataDrivenSelectionSortTest {
+
+//    @Parameterized.Parameter(value = 0)
+//    public int input; //ứng cột 0
+//    @Parameterized.Parameter(value = 1)
+//    public long expected; //ứng cột 1
+//
+//    @Test //tự loop qua các bộ data trong hàm init() ở dưới
+//    public void testFactorialGivenRightArgumentReturnsGoodResult() {
+//        assertEquals(expected, getFactorial(input));
+//    }
+//
+//    //ta phải chuẩn bị bộ data: excel, txt CSV, table trong db
+//    //mảng 2 chiều//
+//    //cần có (đầu vào, đầu ra) nhiều cập như thế 
+//    //quy ước mảng 2 chiều này phải là static
+//    //Singleton - Design Patterns
+//    @Parameterized.Parameters
+//    public static Object[][] initData() {
+//        //int a[] = {1,1,2,3,5,8,13,21}; //mảng 8 phần tử
+//        //8 biến int có sẵn value
+//        return new Integer[][]{
+//            {0, 1},
+//            {1, 1},
+//            {3, 6},
+//            {6, 720},
+//            {7, 5040}
+//        };//chuẩn bị bộ data trước data
+//        //sau đó đưa cho hàm getF(0)
+//        //                      assertEquals(1, ?);
+//        //data driven testing
+//        //"OOP object oriented"
+
+//    }
+
+}
